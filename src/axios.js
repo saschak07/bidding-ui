4083786e3bad8f3ca99ee8130283958f38d2eb08
saswata <saschak07@gmail.com>
2020-04-19 22:59:35 +0530

    axios modifications

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-   // baseURL: 'https://bidding-service.herokuapp.com'
-   baseURL: 'http://localhost:9000/'
+    baseURL: 'https://bidding-service.herokuapp.com'
+   //baseURL: 'http://localhost:9000/'
 })
 
 export default instance;
